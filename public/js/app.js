9f5e07be26aeed8ca77c868dcdfd4a64b8501263
Mattéo Dufour <57948224+mrgenky@users.noreply.github.com>
2022-08-15 17:35:49 +0200

    Fix

--- public/js/app.js
+++ public/js/app.js
@@ -2176,17 +2176,15 @@ vue__WEBPACK_IMPORTED_MODULE_0__["default"].use((vuetify__WEBPACK_IMPORTED_MODUL
   theme: {
     dark: false,
     themes: {
-      light: {
-        primary: '#19c2d4' // secondary: '#b0bec5',
+      light: {// primary: '#19c2d4',
+        // secondary: '#b0bec5',
         // accent: '#8c9eff',
         // error: '#b71c1c',
-
       },
-      dark: {
-        primary: '#19c2d4' // secondary: '#b0bec5',
+      dark: {// primary: '#19c2d4',
+        // secondary: '#b0bec5',
         // accent: '#8c9eff',
         // error: '#b71c1c',
-
       }
     }
   },
--- resources/js/plugins/vuetify.js
+++ resources/js/plugins/vuetify.js
@@ -8,13 +8,13 @@ export default new Vuetify({
         dark:false,
         themes: {
             light: {
-                primary: '#19c2d4',
+                // primary: '#19c2d4',
                 // secondary: '#b0bec5',
                 // accent: '#8c9eff',
                 // error: '#b71c1c',
             },
             dark: {
-                primary: '#19c2d4',
+                // primary: '#19c2d4',
                 // secondary: '#b0bec5',
                 // accent: '#8c9eff',
                 // error: '#b71c1c',
