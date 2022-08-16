5521777bfa06938e622d6119966c8b723e81a6db
Mattéo Dufour <57948224+mrgenky@users.noreply.github.com>
2022-08-16 12:12:44 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ require('./bootstrap');
 import Vue from "vue"
 import vuetify from "./plugins/vuetify"
 import router from './router'
-// import store from './store'
+import store from './store'
 import App from './App.vue';
 
 import 'vuetify/dist/vuetify.min.css'
@@ -13,6 +13,6 @@ import '@mdi/font/css/materialdesignicons.css'
 const app = new Vue({
     vuetify,
     router,
-    // store,
+    store,
     render: (h) => h(App),
 }).$mount('#app');
